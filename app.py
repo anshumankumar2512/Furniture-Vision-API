05f8b47b4f70118f8908d338b6d26dae253160e9
Pourab Karchaudhuri <pourab.karchaudhuri@gmail.com>
2021-06-03 20:41:15 +0530

    stable code

--- app.py
+++ app.py
@@ -1,37 +1,68 @@
 import os
-from flask import Flask,render_template,jsonify,request
-from functions import *
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+from flask import Flask, jsonify,request
+import time
+from handlers import *
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 #Specifying path of targert folder
-app.config["UPLOAD_PATH"] = r"C:\Users\Anshuman\Vision Search Project\uploads"
+UPLOAD_PATH = os.path.join(os.getcwd(), 'upload')
+PROCESSED_PATH = os.path.join(os.getcwd(), 'processed')
 
-
-@app.route("/upload_file",methods=["GET","POST"])
+@app.route("/upload",methods=["GET","POST"])
 def upload_file():
     if request.method == 'POST':
         #if image is uploaded as a local file    
         if (request.headers.get('Content-type').split(';')[0] == 'multipart/form-data'):
 
             # Save the image in uploads folder. Store filepath in a variable and call the pass_file function.
-            
-            for f in request.files.getlist('file_name'):
-                 f.save(os.path.join(app.config['UPLOAD_PATH'],f.filename))
-                 print(os.path.join(app.config['UPLOAD_PATH'],f.filename))
-                 filepath=os.path.join(app.config['UPLOAD_PATH'],f.filename)
-                 result=pass_file(filepath)  
-                 crop_file(result,filepath)
+            if 'image' in request.files.keys():
+            # print("Request Body: " + str(request.files['image'].filename))
+                file = request.files['image']
+                if file.filename == '':
+                    return jsonify('no image received')
+                SAVED_FILE_PATH = os.path.join(UPLOAD_PATH, secure_filename(file.filename))
+                print("File save path : {}".format(SAVED_FILE_PATH))
+                # file.save(file_path, secure_filename(file.filename))
+                file.save(SAVED_FILE_PATH)
+                objects_detected = object_detection_handler(SAVED_FILE_PATH, None) 
+                crop_file(objects_detected, SAVED_FILE_PATH, file.filename, PROCESSED_PATH)
+                # print(result)
+                
+                return jsonify({"status": "success"}), 200
+                
+            else:
+                return jsonify(message_builder("Invalid body", "Please provide an Image as form data with key name 'image'")), 415  
 
         #if image is uploaded as URL
         if request.headers.get("Content-type")=="application/json":
-            
-            URL=request.args.get('url')
-            result=pass_url(URL)
-            crop_url(result,URL)         
+            body = request.get_json()
+            IMAGE_URL = body['url']
 
-        return jsonify({"message":"Uploaded successfully"}, 200 )
+            objects_detected = object_detection_handler(None, IMAGE_URL)
+            
+            image_stream = requests.get(IMAGE_URL, stream=True)
+            image = Image.open(BytesIO(image_stream.content)).convert('RGB')
+            filename = str(time.time()).split('.')[0]
+            SAVED_FILE_PATH = os.path.join(UPLOAD_PATH, secure_filename(filename) + '.jpg')
+            image.save(SAVED_FILE_PATH)
+            crop_file(objects_detected, SAVED_FILE_PATH, filename + '.jpg', PROCESSED_PATH)
+            # crop_url(result,URL)  
+            # print(result)       
+
+        return jsonify({"message":"Uploaded successfully"}), 200
     #return render_template("upload_file.html", msg="Please select file")
 
+def message_builder(argument, message):
+    res = {
+        "error": {
+            "code": argument,
+            "message": message
+        }
+    }
+    return res
+
 if (__name__)=='__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ handlers.py
@@ -0,0 +1,50 @@
+import requests
+import os
+import json
+from PIL import Image
+from io import BytesIO
+
+def object_detection_handler(file_path, file_uri):
+    url = "https://centralindia.api.cognitive.microsoft.com/vision/v3.0/analyze?visualFeatures=Categories,Color,Objects,Description&language=en"
+    payload = dict()
+    files = list()
+    headers = {
+        'Ocp-Apim-Subscription-Key': 'a731048f9a6448a799920bf28d220f2d'
+    }
+    if not (file_uri is None):
+        print("Url exists")
+        payload = json.dumps({
+            "url": file_uri
+        })
+        print("Payload : ", payload)
+
+    elif not (file_path is None):
+        print("Local file")
+        files = [('image', ('file', open(file_path, 'rb'), 'application/octet-stream'))]
+        
+    else:
+        print("Exception")
+
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    result = json.loads(response.text)
+    return result['objects']
+
+def crop_file(objects_detected, source, filename, destination):
+    image = Image.open(source)
+    
+    print("No. of objects detected were : {}".format(len(objects_detected)))
+
+    for items in objects_detected:
+        
+        x = items['rectangle']['x']
+        y = items['rectangle']['y']
+        w = items['rectangle']['w']
+        h = items['rectangle']['h']
+        print('Bounding Box : {}, {}, {}, {}'.format(x, y, w, h))
+        print('Object : {}'.format(items['object']))
+
+        cropped_image = image.crop((x, y, x+w, y+h))
+
+        extension =  filename.split('.')[len(filename.split('.'))-1]
+        cropped_image.save(os.path.join(destination, items['object'] + '.' + extension))
+
